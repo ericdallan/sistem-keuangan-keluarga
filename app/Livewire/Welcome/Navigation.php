552b688caa8bfcac6d0b88acd0092eff94ec6ea1
Subagja Eric Dallan <laramidia.28@gmail.com>
2026-05-01 15:57:36 +0700

    Feat (Layout): add livewire layout and welcome

---
+++ app/Livewire/Welcome/Navigation.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Livewire\Welcome;
+
+use Livewire\Component;
+
+class Navigation extends Component
+{
+    /**
+     * Menampilkan tampilan navigasi.
+     */
+    public function render()
+    {
+        return view('livewire.welcome.navigation');
+    }
+}
